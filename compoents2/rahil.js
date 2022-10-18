c22e35fe592969c60a22e5ac6dfd02f73aa84dcd
Rahil666 <rahilramesh666@gmail.com>
2022-10-18 17:00:42 +0530

    Main

---
+++ compoents2/rahil.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text>rahi;lrahil</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
--- ygdyhf.js
+++ ygdyhf.js
@@ -4,7 +4,7 @@ import React from 'react';
 const App = () => {
   return (
     <View>
-      <Text>Apdejhbdhjfbjkhfjkhfjkp</Text>
+      <Text>App</Text>
     </View>
   );
 };
